1f71b0b5fe14337d498f3b0af69e5cd304a67872
Rezerbiw12 <teerawut1212@hotmail.com>
2020-02-17 11:27:14 +0700

    top appbar

--- lib/hotelscreen.dart
+++ lib/hotelscreen.dart
@@ -15,12 +15,135 @@ class _HotelScreenState extends State<HotelScreen> {
         elevation: 0.0,
         title: Text(
           'Explore',
-          style: TextStyle(fontWeight: FontWeight.bold, color: Colors.black),
+          style: TextStyle(
+              fontWeight: FontWeight.bold, color: Colors.black, fontSize: 25),
         ),
         leading: new IconButton(
           icon: new Icon(Icons.arrow_back, color: Colors.black),
           onPressed: () => Navigator.of(context).pop(),
         ),
+        actions: <Widget>[
+          IconButton(
+              icon: Icon(
+                Icons.favorite_border,
+                color: Colors.black,
+                size: 30,
+              ),
+              onPressed: () {}),
+          IconButton(
+              icon: Icon(
+                Icons.room,
+                color: Colors.black,
+                size: 30,
+              ),
+              onPressed: () {})
+        ],
+      ),
+      body: Column(
+        children: <Widget>[
+          Container(
+            height: 180,
+            color: Colors.black12,
+            child: Column(
+              children: <Widget>[
+                Padding(
+                  padding:
+                      const EdgeInsets.only(top: 16.0, left: 16, right: 16),
+                  child: Row(
+                    children: <Widget>[
+                      Expanded(
+                        flex: 4,
+                        child: TextFormField(
+                          decoration: new InputDecoration(
+                            labelText: "London",
+                            filled: true,
+                            fillColor: Colors.white,
+                            border: new OutlineInputBorder(
+                              borderRadius: new BorderRadius.circular(25.0),
+                              borderSide: new BorderSide(),
+                            ),
+                          ),
+                        ),
+                      ),
+                      Expanded(
+                        flex: 1,
+                        child: Container(
+                          child: GestureDetector(
+                              onTap: () {},
+                              child: new Container(
+                                height: 50.0,
+                                width: 50.0,
+                                child: Center(
+                                    child: Icon(
+                                  Icons.search,
+                                  color: Colors.white,
+                                )),
+                                decoration: BoxDecoration(
+                                  shape: BoxShape.circle,
+                                  color: Colors.greenAccent,
+                                  boxShadow: [
+                                    BoxShadow(
+                                      color: Colors.grey,
+                                      blurRadius: 5.0,
+                                      offset: Offset(5.0, 5.0),
+                                    )
+                                  ],
+                                ),
+                              )),
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.only(top: 8, left: 8, right: 8),
+                  child: Row(
+                    children: <Widget>[
+                      Expanded(
+                        child: Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: <Widget>[
+                              Text('Choose Date'),
+                              Text(
+                                '12 Dec - 22 Dec',
+                                style: TextStyle(
+                                    fontSize: 18, fontWeight: FontWeight.bold),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                      Expanded(
+                        child: Row(
+                          children: <Widget>[
+                            Container(
+                              height: 60,
+                              child: VerticalDivider(color: Colors.black26),
+                            ),
+                            Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: <Widget>[
+                                Text('Number of Rooms'),
+                                Text(
+                                  '1 Room - 2 Adults',
+                                  style: TextStyle(
+                                      fontSize: 18,
+                                      fontWeight: FontWeight.bold),
+                                ),
+                              ],
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                )
+              ],
+            ),
+          )
+        ],
       ),
     );
   }
